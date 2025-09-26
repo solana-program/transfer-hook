9a4d29e0e047d0ca23d0f23d5978de33272c05fc
Jon C <me@jonc.dev>
2025-09-26 23:54:33 +0200

    Gate program tests on required feature

--- program/tests/functional.rs
+++ program/tests/functional.rs
@@ -1,6 +1,6 @@
 // Mark this test as SBF-only due to current `ProgramTest` limitations when
 // CPIing into the system program
-#![cfg(feature = "test-sbf")]
+#![cfg(all(feature = "test-sbf", feature = "forbid-additional-mints"))]
 
 use {
     solana_program_test::{processor, tokio, ProgramTest},
