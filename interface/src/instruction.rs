57f436dc607516b77140e6b05c9849509bdd9a63
Jon C <me@jonc.dev>
2026-09-03 13:14:11 +0200

    deps: Bump tlv-account-resolution, fix issues
    #### Problem
    
    The dependencies here are a bit old, especially for tlv account
    resolution.
    
    #### Summary of changes
    
    Bump the dependencies, and fix any issues that come up. In particular,
    we demote all signers now, so make sure the tests reflect that.

--- interface/src/instruction.rs
+++ interface/src/instruction.rs
@@ -5,7 +5,7 @@ use {
     solana_program_error::ProgramError,
     solana_pubkey::Pubkey,
     spl_discriminator::{ArrayDiscriminator, SplDiscriminate},
-    spl_pod::{bytemuck::pod_slice_to_bytes, list::ListView},
+    spl_list_view::ListView,
     spl_tlv_account_resolution::account::ExtraAccountMeta,
     std::convert::TryInto,
 };
@@ -129,7 +129,7 @@ impl TransferHookInstruction {
                     InitializeExtraAccountMetaListInstruction::SPL_DISCRIMINATOR_SLICE,
                 );
                 buf.extend_from_slice(&(extra_account_metas.len() as u32).to_le_bytes());
-                buf.extend_from_slice(pod_slice_to_bytes(extra_account_metas));
+                buf.extend_from_slice(bytemuck::cast_slice(extra_account_metas));
             }
             Self::UpdateExtraAccountMetaList {
                 extra_account_metas,
@@ -138,7 +138,7 @@ impl TransferHookInstruction {
                     UpdateExtraAccountMetaListInstruction::SPL_DISCRIMINATOR_SLICE,
                 );
                 buf.extend_from_slice(&(extra_account_metas.len() as u32).to_le_bytes());
-                buf.extend_from_slice(pod_slice_to_bytes(extra_account_metas));
+                buf.extend_from_slice(bytemuck::cast_slice(extra_account_metas));
             }
         };
         buf
@@ -252,9 +252,7 @@ pub fn update_extra_account_meta_list(
 
 #[cfg(test)]
 mod test {
-    use {
-        super::*, crate::NAMESPACE, solana_sha256_hasher::hashv, spl_pod::bytemuck::pod_from_bytes,
-    };
+    use {super::*, crate::NAMESPACE, solana_sha256_hasher::hashv};
 
     #[test]
     fn system_program_id() {
@@ -291,7 +289,7 @@ mod test {
             0, // is_writable
         ];
         let extra_account_metas =
-            vec![*pod_from_bytes::<ExtraAccountMeta>(extra_meta_bytes).unwrap()];
+            vec![*bytemuck::try_from_bytes::<ExtraAccountMeta>(extra_meta_bytes).unwrap()];
         let check = TransferHookInstruction::InitializeExtraAccountMetaList {
             extra_account_metas,
         };
